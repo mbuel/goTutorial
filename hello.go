ff01b2044554b1fa754a7b10d9aeedb35e11174c
Morris Buel <morris.buel@gmail.com>
2020-05-13 15:14:36 -0700

    arrays

--- hello.go
+++ hello.go
@@ -6,5 +6,36 @@ import (
 
 func main()  {
 	fmt.Println("hello!")
-	
+	x := 7
+
+	if x > 6 {
+		fmt.Println("More than 6")
+	}
+
+	// a := [5]int{5,4,3,2,1}  // Array
+	// fmt.Println(a)
+
+	a := []int{5,4,3,2,1} // Slice
+	fmt.Println(a)
+
+	a = append(a, 13)
+	fmt.Println(a)
+
+	// Map
+	vertices := make(map[string]int)
+
+	vertices["triangle"] = 2
+	vertices["square"] = 3
+	vertices["dodecagon"] = 12
+
+	delete(vertices, "square")
+	fmt.Println(vertices)
+
+	for i:= 0; i<5; i++ {
+		fmt.Println(i)
+	}
+
+	for index, value := range a {
+		fmt.Println("index: ", index, "value: ", value)
+	}
 }
